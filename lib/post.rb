3f16fe16f5695f0856a72a6f613c89ce62d7d426
Olesya Miller <olesyam.miller@gmail.com>
2019-12-22 00:07:07 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,18 @@
+class Post 
+  attr_accessor :name, :author, :title 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def author_name 
+    author.name if self.author  
+  end
+end
