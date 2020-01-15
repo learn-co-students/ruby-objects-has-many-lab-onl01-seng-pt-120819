30f96ed2d817ba8cf323178c762d78388f3e244e
cedric e goddard <8ceg6@comcast.net>
2020-01-15 05:36:55 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -25,16 +25,16 @@ class Artist
  
  def add_song_by_name(song_name)
      new_song = Song.new(song_name)
-      #self.add_song(new_song)
+      self.add_song(new_song)
     
-    @songs << new_song 
-    new_song.artist = self
-    @@song_count += 1
-    binding.pry
+    # @songs << new_song 
+    # new_song.artist = self
+    # @@song_count += 1
+    # binding.pry
  end
  
  def self.song_count
-      @@song_count 
+      Song.all.count 
       
  end 
 end 
