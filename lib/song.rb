70fc64b2651e05e417aeb2300b3d251682e93837
Asif Kamal <asif_kamal@hotmail.com>
2020-01-18 20:31:21 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -17,11 +17,8 @@ class Song
   end
   
   def artist_name
-    if self.artist.name == nil 
-      nil
-    else
-      self.artist.name
-    end
+    self.artist.name unless self.artist.nil?
+
   end
   
   
