31f2864866e3536dbb41f14e15afa515eabafc0d
Asif Kamal <asif_kamal@hotmail.com>
2020-01-18 20:23:05 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,10 +11,15 @@ class Artist
   
   def add_song_by_name(name)
     song = Song.new(name)
-  
+    add_song(song)
   end
  
   def songs
     Song.all.select {|song| song.artist == self}
   end
+  
+  def self.song_count
+     Song.all.count 
+   end
+  
 end
