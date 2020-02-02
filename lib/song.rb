7792d93b26819d0c87870be81efb503520ef51cf
Brigette LeVert <bnl.levert@gmail.com>
2020-02-02 20:40:12 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,24 +1,37 @@
 # class Song 
-# attr_accessor :artist, :name
+
+# attr_accessor :name, :artist 
+
+# @@all = []
 
 #   def initialize(name)
 #     @name = name
-  
-  
+#     @artist = artist 
+#     @@all << self 
 #   end 
   
 # end
 
 class Song 
+  attr_accessor :name, :artist 
 
-attr_accessor :name, :artist 
+  @@all = []
 
-@@all = []
-
-  def initialize(name)
+  def initialize(name, artist=nil)
     @name = name
-    @artist = artist 
-    @@all << self 
-  end 
-  
+    @artist = artist
+    @@all << self
+  end
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil
+    end
+  end
+
+  def self.all
+    @@all
+  end
 end
